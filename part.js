9c549cf8e12828d095b8788047c8a419e7fdc309
Admin <maxml@inbox.ru>
2020-03-23 16:00:47 +0200

    new fix for next days

--- part.js
+++ part.js
@@ -3,51 +3,51 @@ $(document).ready(function() {
   const Month = date.getMonth() + 1;
   const Year = date.getFullYear();
 
-  //  $('form').on('DOMSubtreeModified', '.t-input-block_inited-date-picker', function() {
-  $('.today').prevAll('tbody tr>td').addClass('pastDay');
-  $('.today').parent('tr').prevAll('tr').addClass('pastDay');
+  $('form').on('DOMSubtreeModified', '.t-input-block_inited-date-picker', function() {
+    $('.today').prevAll('tbody tr>td').addClass('pastDay');
+    $('.today').parent('tr').prevAll('tr').addClass('pastDay');
 
-  $('.today').nextAll('tbody tr>td').addClass('pastDay');
-  $('.today').parent('tr').next().nextAll('tr').addClass('pastDay');
+    $('.today').nextAll('tbody tr>td').addClass('pastDay');
+    $('.today').parent('tr').next().nextAll('tr').addClass('pastDay');
 
-  $('.today').addClass('pastDay');
+    $('.today').addClass('pastDay');
 
-  const current = $('.today').text();
-  $.each($('.today').parent('tr'), function(i, it) {
-    var $this = $(this);
-    var $items = $this.find("td");
-
-    $.each($items, function(index, item) {
+    const current = $('.today').text();
+    $.each($('.today').parent('tr'), function(i, it) {
       var $this = $(this);
+      var $items = $this.find("td");
 
-      if ($(this).text() === current) {
-        var $thisTd = $(this);
-        console.log('10')
-        $this.removeClass('pastDay')
-        $this.each(function() {
-          $(this).addClass('pastDay')
-        });
-        removeClassNextTwoDays($thisTd);
-      }
-    })
+      $.each($items, function(index, item) {
+        var $this = $(this);
+
+        if ($(this).text() === current) {
+          var $thisTd = $(this);
+          console.log('10')
+          $this.removeClass('pastDay')
+          $this.each(function() {
+            $(this).addClass('pastDay')
+          });
+          removeClassNextTwoDays($thisTd);
+        }
+      })
+    });
+    if ($('.today').is(':last-child')) {
+      markNextElements($('.today').parent('tr').next(), 2);
+    } else if ($('.today').next().is(':last-child')) {
+      markNextElements($('.today').parent('tr').next(), 1);
+    } else if ($('.today').next().next().is(':last-child')) {
+      markNextElements($('.today').parent('tr').next(), 0);
+    } else {
+      markNextElements($('.today').parent('tr').next(), 0);
+    }
+
+    var picM = $('select.dp-select-month option:selected').val();
+    var picD = $('select.dp-select-year option:selec2ted').val();
+    if (picM < Month && (picD <= Year || picD < Year)) {
+      $('.cal-month tr').addClass('pastDay');
+    }
   });
-  if ($('.today').is(':last-child')) {
-    markNextElements($('.today').parent('tr').next(), 2);
-  } else if ($('.today').next().is(':last-child')) {
-    markNextElements($('.today').parent('tr').next(), 1);
-  } else if ($('.today').next().next().is(':last-child')) {
-    markNextElements($('.today').parent('tr').next(), 0);
-  } else {
-    markNextElements($('.today').parent('tr').next(), 0);
-  }
-
-  var picM = $('select.dp-select-month option:selected').val();
-  var picD = $('select.dp-select-year option:selec2ted').val();
-  if (picM < Month && (picD <= Year || picD < Year)) {
-    $('.cal-month tr').addClass('pastDay');
-  }
-});
-//})
+})
 
 function markNextElements(tr, count) {
   $.each(tr, function(i, it) {
