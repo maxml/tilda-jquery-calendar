9415ab4307dac4ebb80a2797768e41d7c9da5bd1
Admin <maxml@inbox.ru>
2020-03-23 13:51:21 +0200

    only next two open days

--- part.js
+++ part.js
@@ -3,8 +3,7 @@ $(document).ready(function() {
   const Month = date.getMonth() + 1;
   const Year = date.getFullYear();
 
-//  $('form').on('DOMSubtreeModified', '.t-input-block_inited-date-picker', function() {
-
+  $('form').on('DOMSubtreeModified', '.t-input-block_inited-date-picker', function() {
     $('.today').prevAll('tbody tr>td').addClass('pastDay');
     $('.today').parent('tr').prevAll('tr').addClass('pastDay');
 
@@ -13,6 +12,7 @@ $(document).ready(function() {
 
     $('.today').addClass('pastDay');
 
+    const current = $('.today').text();
     $.each($('.today').parent('tr'), function(i, it) {
       var $this = $(this);
       var $items = $this.find("td");
@@ -20,14 +20,16 @@ $(document).ready(function() {
       $.each($items, function(index, item) {
         var $this = $(this);
 
-        if ($(this).text() == 10) {
+        if ($(this).text() === current) {
           var $thisTd = $(this);
           console.log('10')
           $this.removeClass('pastDay')
           $this.each(function() {
             $(this).addClass('pastDay')
           })
-          $thisTd.removeClass('pastDay')
+          /* $thisTd.removeClass('pastDay') */
+
+          removeClassNextTwoDays($thisTd);
         }
       })
     });
@@ -37,35 +39,28 @@ $(document).ready(function() {
     if (picM < Month && (picD <= Year || picD < Year)) {
       $('.cal-month tr').addClass('pastDay');
     }
-
-    // if($('.today').is(':last-child')){
-    //       $('.today').closest('tr').next('tr').find('td:first-child').addClass('pastDay') ;
-    //   } else {
-    //     $('.today').closest('td').next('td').addClass('pastDay');
-    //   }
-
-    /*
-      if ($('.today').is(':last-child')) {
-        $last = $('.today').closest('tr').next('tr').find('td:first-child');
-        $last.addClass('pastDay');
-      
-        if ($last.is(':last-child')) {
-          $last.closest('tr').next('tr').find('td:first-child').addClass('pastDay');
-        } else {
-          $last.closest('td').next('td').addClass('pastDay');
-        }
-      } else {
-        $last = $('.today').closest('td').next('td');
-      
-        $last.addClass('pastDay');
-        if ($last.is(':last-child')) {
-          $last.closest('tr').next('tr').find('td:first-child').addClass('pastDay');
-        } else {
-          $last.closest('td').next('td').addClass('pastDay');
-        }
-      }
-       */
-
-//  });
+  });
 })
+
+function removeClassNextTwoDays(neededTd) {
+  if (neededTd.is(':last-child')) {
+    $last = neededTd.closest('tr').next('tr').find('td:first-child');
+    $last.removeClass('pastDay');
+
+    if ($last.is(':last-child')) {
+      $last.closest('tr').next('tr').find('td:first-child').removeClass('pastDay');
+    } else {
+      $last.closest('td').next('td').removeClass('pastDay');
+    }
+  } else {
+    $last = neededTd.closest('td').next('td');
+
+    $last.removeClass('pastDay');
+    if ($last.is(':last-child')) {
+      $last.closest('tr').next('tr').find('td:first-child').removeClass('pastDay');
+    } else {
+      $last.closest('td').next('td').removeClass('pastDay');
+    }
+  }
+}
 
